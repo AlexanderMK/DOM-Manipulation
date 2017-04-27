8a561532c41354f3616ab89d1e4d2fe3684ca9a3
Alexander <mburukungu@gmail.com>
2017-04-27 08:31:26 +0300

    edited scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
         $("button#hello").click(function() {
-          $("ul").prepend("<li>Hello!</li>");
+          $("ul").before("<li>Hello!</li>");
         });
 
         $("button#goodbye").click(function() {
-          $("ul").prepend("<li>Goodbye!</li>");
+          $("ul").append("<li>Goodbye!</li>");
         });
 
         $("button#stop").click(function() {
-          $("ul").prepend("<li>Stop copying me!</li>");
+          $("ul").append("<li>Stop copying me!</li>");
         });
       });
